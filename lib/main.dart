87cc2cd4e92642d0a15d7ec0d4883e144ac6d6ee
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-07-24 00:40:41 +0300

    connect with firebase intgration 🔥

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:restaurant/config/router.dart';
+import 'package:restaurant/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const FoodieApp());
 }
 
