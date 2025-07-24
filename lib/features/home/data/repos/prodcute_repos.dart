7c5d8c65190c786d03180b990f1b2b3bdf04122c
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-07-24 23:07:51 +0300

    product data repo

---
+++ lib/features/home/data/repos/prodcute_repos.dart
@@ -0,0 +1,22 @@
+import 'package:restaurant/features/home/data/models/fast_food_model.dart';
+
+List<FastFoodModel> fastFoods = [
+  FastFoodModel(
+    image:
+        "https://images.getrecipekit.com/20240515201802-grilled-cheeseburgers.webp?width=650&quality=90&",
+    name: 'Burger',
+    price: '20',
+  ),
+  FastFoodModel(
+    image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Pizza-3007395.jpg/1280px-Pizza-3007395.jpg",
+    name: 'Pizza',
+    price: '30',
+  ),
+  FastFoodModel(
+    image:
+        "https://latourangelle.com/cdn/shop/articles/hikynvl8pjkjqhvpnok6_150x.jpg?v=1619198610",
+    name: 'Fries',
+    price: '10',
+  ),
+];
