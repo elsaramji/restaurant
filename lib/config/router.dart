a11acd66eb95db64b93c539ed27c6a44c64dae0f
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-07-24 23:03:17 +0300

    add home and cart routes

--- lib/config/router.dart
+++ lib/config/router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:restaurant/features/auth/presentiton/views/login_screen.dart';
+import 'package:restaurant/features/cart/presentition/views/foodie_cart_view.dart';
 import 'package:restaurant/features/home/presentition/view/foodei_home.dart';
 
 final GoRouter router = GoRouter(
@@ -15,6 +16,11 @@ final GoRouter router = GoRouter(
       path: FoodeiHome.routeName,
       name: FoodeiHome.routeName,
       pageBuilder: (context, state) => _easyTransition(const FoodeiHome()),
+    ),
+    GoRoute(
+      path: FoodieCartView.routeName,
+      name: FoodieCartView.routeName,
+      pageBuilder: (context, state) => _easyTransition(const FoodieCartView()),
     ),
   ],
 );
