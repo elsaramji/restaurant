0662d089231b3140dbb35624d11e72db4601b0ca
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-07-24 23:01:21 +0300

    add app background color

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const white = Color(0xffFFffff);
+  static const scaffoldBackground = Color(0xffF5F5F5);
   static const primary = Color(0xffFF6B35);
   static const primaryText = Color(0xff111827);
   static const secondaryform = white;
@@ -9,7 +10,4 @@ class AppColors {
   static const secondaryText = Color(0xff4B5563);
   static const secondarysubText = Color(0xff6B7280);
   static const secondaryfillred = Color(0xffF87171);
-
-  
-
 }
